3d3aa7d6d5330acaf61226fbea6b9407d14bc818
Nelson Umunna <nelson.umunna@carleton.ca>
2021-02-16 13:40:48 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome Nelson Umunna Volumes with docker-compose.
         </p>
         <a
           className="App-link"
