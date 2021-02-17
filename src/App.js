f4f470e9ce615572b74272edb8d324dc60d10a8d
nelobaba <nelson.umunna@carleton.ca>
2021-02-16 21:58:06 -0500

    Changed a file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome Nelson Umunna.
+          Welcome Nelson Umunna. 
+          This is a sample CICD workflow
         </p>
         <a
           className="App-link"
